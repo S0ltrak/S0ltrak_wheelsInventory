28087aef81a63a0ca438ce0a4e934568aebc9126
S0ltrak <s0ltrak@tutamail.com>
2024-11-11 20:52:32 +0100

    Update cl_wheels.lua

--- s0ltrak_wheelsInventory/cl_wheels.lua
+++ s0ltrak_wheelsInventory/cl_wheels.lua
@@ -1,8 +1,13 @@
 function ToggleWeaponWheel(state)
     if type(state) == "boolean" then
-        exports.ox_inventory:weaponWheel(state)
+        if exports.ox_inventory then
+            exports.ox_inventory:weaponWheel(state)
+        else
+            print("Erreur: ox_inventory n'est pas disponible.")
+        end
     else
         print("Erreur: Le paramètre doit être un booléen (true pour activer, false pour désactiver).")
     end
 end
+
 ToggleWeaponWheel(true)
